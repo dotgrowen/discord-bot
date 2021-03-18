de4ba9dddb8f441cb44765f784fc54337a0ae56e
Dot Growen <60196575+dotgrowen@users.noreply.github.com>
2021-03-18 04:29:24 -0500

    testing commands

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-// const config = require('./config.json')
+
+const config = require('./config.json')
+const command = require('./command.js')
 
 client.on('ready', () => {
-    console.log("The client is ready!")
+    console.log('The client is ready!')
+
+    command(client, ['ping', 'test'], (message) => {
+        console.log("pong")
+        message.channel.send('Pong!')
+    })
 })
 
-client.login(process.env.DJS_TOKEN)
+client.login(config.token)
+// client.login(process.env.DJS_TOKEN)
